571ee3e6a2e27fa45424a23c0082ecf632a9ccfd
Gio <gio.busrom@gmail.com>
2020-09-09 11:40:13 -0600

    Initial Commit

---
+++ Hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hw3
+//
+//  Created by IS543 on 9/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hw3/Hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  Hw3App.swift
+//  Hw3
+//
+//  Created by IS543 on 9/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
