938aeb38c54316f20f546872c5bd334d5c329a0d
Gio <gio.busrom@gmail.com>
2020-09-26 10:24:15 -0600

    Ad

--- Hw3/View/Menu.swift
+++ Hw3/View/Menu.swift
@@ -9,24 +9,54 @@ import SwiftUI
 
 struct Menu: View {
     private var EmojiMojoThemes = ["Halloween","Fruits","Emojis","Animals","Music","Cars"]
-    private var TempleThemes = ["SouthAmerica","Utah","Asia","Europe","NorthAmerica","Africa"]
+//    private var TempleThemes = ["SouthAmerica","Utah","Asia","Europe","NorthAmerica","Africa"]
+    private var gamesList = ["Emoji Mojo", "Temple Match", "Shape Match"]
+    @State var selectedGame = 0
+    @State var isActive = false
+
 
     var body: some View {
-        VStack{
-            Text("Concentration Games")
-                .bold()
-                .font(.system(size: 40))
-                .multilineTextAlignment(.center)
-                .padding()
-                .font(.system(size: 20))
-            HStack{
-                OptionMenuView(nameGameTitle:"Emoji Mojo", Themes: EmojiMojoThemes)
-                OptionMenuView(nameGameTitle:"Temple Match", Themes: TempleThemes)
+        NavigationView {
+
+            VStack{
+                Text("Concentration Games")
+                    .bold()
+                    .font(.system(size: 40))
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Text("select a game:")
+                    .padding()
+                    .font(.system(size: 20))
+                Picker(selection: $selectedGame, label: Text("Select Your Story")) {
+                    ForEach(0 ..< gamesList.count) { gameli in
+                        NavigationLink(gamesList[gameli],
+                                       destination: OptionMenuView(nameGameTitle:"Emoji Mojo", Themes: EmojiMojoThemes),
+                                       isActive: $isActive)
+                    }
+                }
+                .onChange(of: selectedGame, perform: { _ in
+                    isActive.toggle()
+                })
+                
+                
+                
+//                Picker("Number of Pairs", selection: $selectedGame){
+//                    ForEach(0 ..< gamesList.count) {
+//                        Text(self.gamesList[$0])
+//                    }
+//                }
+//                .labelsHidden()
+//                .frame(width: 150, height: 30, alignment: .center)
+//                .padding()
+                
+    //            HStack{
+    //                OptionMenuView(nameGameTitle:"Emoji Mojo", Themes: EmojiMojoThemes)
+    //                OptionMenuView(nameGameTitle:"Temple Match", Themes: TempleThemes)
+    //            }
+    //
+    //            OptionMenuView(nameGameTitle:"Shape Match", Themes: TempleThemes)
+
             }
-
-            OptionMenuView(nameGameTitle:"Shape Match", Themes: TempleThemes)
-
-
         }
 
     }
